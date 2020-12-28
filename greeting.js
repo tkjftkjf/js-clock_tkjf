f7498d605bac450b64913173cee993a579362e5a
tkjftkjf <tkjftkjf@user.github.com>
2020-12-28 23:43:34 +0900

    greeting complete

---
+++ greeting.js
@@ -0,0 +1,42 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector(".js-form-input"),
+    greeting = document.querySelector(".js-greeting")
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing"
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN)
+    greeting.classList.add(SHOWING_CN)
+    greeting.innerText = ` Hello! ${text}`
+}
+
+function saveName(name){
+    localStorage.setItem(USER_LS ,name)
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN)
+    form.addEventListener("submit", handleSubmit)
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null || currentUser === "") {
+        askForName();
+    } else {
+        paintGreeting(currentUser);
+}
+}
+function init(){
+    loadName()
+}
+
+init()
